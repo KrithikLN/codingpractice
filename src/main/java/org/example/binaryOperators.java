fc0e2d4991f2f16568f06e6c1e37decf7f90a74a
Krithik <krithiklakshminarasimhan@gmail.com>
2024-12-03 09:48:57 +0530

    added Bitwise XOR

--- src/main/java/org/example/binaryOperators.java
+++ src/main/java/org/example/binaryOperators.java
@@ -8,18 +8,24 @@ public class binaryOperators {
         int num, n;
         System.out.print("Enter an integer: ");
         num = sc.nextInt();
-        System.out.print("Enter number to shift: ");
-        n = sc.nextInt();
         int choice;
-        System.out.print("Enter a choise : 1 for left shift; 2 for right shift ");
+        System.out.println("Enter a choise : 1 for left shift; 2 for right shift and 3 for Bitwise XOR ");
         choice = sc.nextInt();
         switch (choice) {
             case 1:
+                System.out.print("Enter number to shift: ");
+                n = sc.nextInt();
                 System.out.print("Left shift : "+leftShiftBinary(num, n));
                 break;
             case 2:
+                System.out.print("Enter number to shift: ");
+                n = sc.nextInt();
                 System.out.print("Right shift : "+rightShiftBinary(num, n));
                 break;
+            case 3:
+                System.out.print("Enter number to Bitwise XOR: ");
+                int x = sc.nextInt();
+                System.out.println("Bitwise XOR : "+bitwiseXOR(num, x));
             default:
                 System.out.println("Invalid choice");
                 break;
@@ -31,6 +37,10 @@ public class binaryOperators {
     }
     public static int rightShiftBinary(int a, int b) {
         int output = a >> b;
+        return output;
+    }
+    public static int bitwiseXOR(int a, int b) {
+        int output = a ^ b;
         return output;
     }
 }
